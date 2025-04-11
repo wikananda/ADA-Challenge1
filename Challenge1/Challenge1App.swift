e0c3e015aefcf5291643ba66a59e926e268615bf
wikananda <nandasantana276@gmail.com>
2025-04-11 14:13:13 +0800

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Komang Wikananda on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1Tests/Challenge1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1Tests.swift
+//  Challenge1Tests
+//
+//  Created by Komang Wikananda on 11/04/25.
+//
+
+import Testing
+@testable import Challenge1
+
+struct Challenge1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
